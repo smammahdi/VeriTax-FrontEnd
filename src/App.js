5b9c0ec584aa03adbad42ff43b88093dbc1c4847
Md. Ishrak Ahsan <1905045@ugrad.cse.buet.ac.bd>
2023-09-05 07:28:05 +0600

    Add basic layout of salary form
    Also added routing from homepage to salary form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import SalaryForm from './pages/SalaryForm';
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Header />
-      <Main />
-      <Footer />
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <>
+                <Navbar />
+                <Header />
+                <Link to="/">Home</Link>
+                <Link to="/salary">Fill up Salary form</Link>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/salary" element={<SalaryForm />} />
+                </Routes>
+                <Footer />
+            </>
+        </BrowserRouter>
+    );
 }
 
 export default App;
