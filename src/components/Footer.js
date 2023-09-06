75fab5cfe03af76233059689698e8418d637abc6
Riad Ahmed Anonto <1905050@ugrad.cse.buet.ac.bd>
2023-09-06 08:28:40 +0600

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,31 +10,31 @@ function Footer() {
           <div className="side1">
             <div className="row">
               <div className="col-md-3">
-                <h1 className="logo">DarkLeas</h1>
+                <h1 className="logo">VeriTax</h1>
                 <p className="footer-text">
                   Lorem ipsum Here are thriteen health benefits of apples Lorem
-        
+
                 </p>
               </div>
               <div className="col-md-3">
                 <p className="footer-title">Important Link</p>
                 <ul>
                   <li>
-                  <Link   
-                         spy={true}
-                         smooth={true}
-                         duration={1000}
-                         to="headerbg"
-                  > Home </Link>
+                    <Link
+                      spy={true}
+                      smooth={true}
+                      duration={1000}
+                      to="headerbg"
+                    > Home </Link>
                   </li>
                   <li>
-                  <Link to="services" spy={true} smooth={true} duration={1000} > Services </Link>
+                    <Link to="services" spy={true} smooth={true} duration={1000} > Services </Link>
                   </li>
                   <li>
-                  <Link to="about-scroll" spy={true} smooth={true} duration={1000}>About Us  </Link>
+                    <Link to="about-scroll" spy={true} smooth={true} duration={1000}>About Us  </Link>
                   </li>
                   <li>
-                  <Link to="contact" spy={true} smooth={true} duration={1000}> Contact  </Link>
+                    <Link to="contact" spy={true} smooth={true} duration={1000}> Contact  </Link>
                   </li>
                 </ul>
               </div>
